a79d44d4b49ebe047c64070799882505e3747967
Richard <richard.ryu@ischool.berkeley.edu>
2020-01-20 19:54:55 -0500

    typos in shell scripts

--- hw03/cloud.sh
+++ hw03/cloud.sh
@@ -4,10 +4,10 @@
 docker network create --driver bridge cbridge
 
 # Build cloud broker container
-docker build -t c-broker -f Dockerfile.cbroker
+docker build -t c-broker -f Dockerfile.cbroker .
 
 # Build cloud face container
-docker build -t c-face -f Dockerfile.cface
+docker build -t c-face -f Dockerfile.cface .
 
 # Run cloud broker container
 docker run -d --name c-broker -p 1883:1883 --network cbridge c-broker
--- hw03/edge.sh
+++ hw03/edge.sh
@@ -2,13 +2,13 @@
 docker network create --driver bridge tx-bridge
 
 # Building Broker Docker
-docker build -t tx-broker -f Dockerfile.broker
+docker build -t tx-broker -f Dockerfile.broker .
 
 # Building Forwarder Docker
-docker build -t tx-forward -f Dockerfile.forward
+docker build -t tx-forward -f Dockerfile.forward .
 
 # Building Face Detection Docker
-docker build -t tx-face -f Dockerfile.face
+docker build -t tx-face -f Dockerfile.face .
 
 # Running Broker Docker
 docker run -d --name tx-broker -p 1883:1883 --network tx-bridge tx-broker
